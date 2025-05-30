cd75ca1f71cb09458540dc846e4db3a4e3dad219
Dhanush05-git <dhanushgundu2005@gmail.com>
2025-05-30 13:08:21 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -15,10 +15,11 @@ export default function SignInPage() {
           // You can add your background image style here or via Tailwind config
         >
           <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center items-center text-white p-10">
-            <h1 className="text-4xl font-bold mb-4">
-              Welcome to AI Interview Mocker{" "}
-              <span className="animate-custom-bounce text-yellow-400">🚀</span>
-            </h1>
+          <h1 className="text-4xl font-bold mb-4">
+            Welcome to AI Interview Mocker{" "}
+            <span className="animate-custom-bounce text-yellow-400">🚀</span>
+          </h1>
+
 
             <p className="text-lg text-center max-w-md">
               Prepare for interviews smarter with AI! Practice, improve, and succeed.
