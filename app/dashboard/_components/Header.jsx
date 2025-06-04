971e43e0c70f1a9105da7271c74788bd5089373b
Dhanush05-git <dhanushgundu2005@gmail.com>
2025-06-04 22:57:11 +0530

    Update Header.jsx

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -27,7 +27,7 @@ function Header() {
               <Image
                 src="/logo.svg"
                 alt="Logo"
-                width={500}
+                width={650}
                 height="auto"
                 priority
                 className="object-contain"
